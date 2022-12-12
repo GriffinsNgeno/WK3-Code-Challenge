64b3a3e6214fdbbe209253ca4f951e50ab1e9da5
Griffins Ngeno <griffins.ngeno@student.moringaschool.com>
2022-12-12 23:54:41 +0300

    Update index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+let form = document.getElementById('myForm')
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+
+    let search = document.getElementById('search').value 
+    // alert(search)
+
+    fetch('')
+})
