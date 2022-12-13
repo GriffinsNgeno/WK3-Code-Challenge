6b5e08bf92c0a99b8c5eb4a78de3da101686b059
Griffins Ngeno <griffins.ngeno@student.moringaschool.com>
2022-12-13 21:49:41 +0300

    Created fetch function

--- src/index.js
+++ src/index.js
@@ -4,3 +4,14 @@ document.addEventListener('DOMContentLoaded', ()=>{
     document.getElementsByClassName('film item')[0].remove()
     fetchMovies(URL)
 })
+
+//Create fetch function to get the data from the db.json
+function fetchMovies(URL){
+    fetch(URL)
+    .then(resp => resp.json())
+    .then(movies => {
+        movies.forEach(movie => {
+            displayMovie(movie)
+        });
+    })
+}
