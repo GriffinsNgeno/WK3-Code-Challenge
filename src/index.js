63c7b224fa59249c7e25ef0dab91665d447bfc72
Griffins Ngeno <griffins.ngeno@student.moringaschool.com>
2022-12-13 21:53:53 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -61,3 +61,16 @@ function setUpMovieDetails(funMovie){
     const tickets  = document.querySelector('#ticket-number')
     tickets.textContent = funMovie.capacity - funMovie.tickets_sold;
 }
+
+//Sold out
+const btn = document.getElementById('buy-ticket')
+        btn.addEventListener('click', function(event){
+            let remainingTickets = document.querySelector('#ticket-number').textContent
+            event.preventDefault()
+            if(remainingTickets > 0){
+                document.querySelector('#ticket-number').textContent  = remainingTickets-1
+            }
+            else if(parseInt(remTickets, 10)===0){
+                btn.textContent = 'Sold Out'
+            }
+    })
